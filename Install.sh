214e8a3e77a5999d27e0cc1536d82d044fd88251
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-20 23:24:06 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,5 +1,11 @@
 mkfs.fat -F32 /dev/sda1 & mkfs.btrfs -f /dev/sda2
-mount /dev/sda2 /mnt & btrfs subvolume create /mnt/@root & btrfs subvolume create /mnt/@home & umount /mnt
-mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@root /dev/sda2 /mnt & mkdir /mnt/home & mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@home /dev/sda2 /mnt/home
-mkdir /boot/efi & mount /dev/sda1 /boot/efi
+mount /dev/sda2 /mnt
+btrfs subvolume create /mnt/@root 
+btrfs subvolume create /mnt/@home 
+umount /mnt
+mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@root /dev/sda2 /mnt 
+mkdir /mnt/home 
+mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@home /dev/sda2 /mnt/home
+mkdir /boot/efi 
+mount /dev/sda1 /boot/efi
 
