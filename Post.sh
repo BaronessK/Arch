0c72e210aeb43f73285849ca6aa92ba1e83bd927
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-21 00:15:42 +0300

    Create Post.sh

---
+++ Post.sh
@@ -0,0 +1,20 @@
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=ru_RU.UTF-8" > /etc/locale.conf
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc --utc
+date
+echo Nikita > /etc/hostname 
+echo "127.0.1.1 localhost.localdomain Nikita" > /etc/hosts
+pacman -S networkmanager
+systemctl enable NetworkManager 
+passwd
+123
+123
+pacman -S grub efibootmgr
+useradd -m -g users -G wheel -s /bin/bash nikita
+passwd nikita
+123
+123
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi --removable
+grub-mkconfig -o /boot/grub/grub.cfg
