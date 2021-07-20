86c9fcd07d0a3b47e6c527c2bd6f971c4b8f974f
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-20 22:54:34 +0300

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,14 @@
+#sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com & pacman-key --lsign-key 3056513887B78AEB
+#pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+mkfs.fat -F32 /dev/sda1
+mkfs.btrfs /dev/sda2 -f
+mount /dev/sda2 /mnt
+btrfs subvolume create /mnt/@root
+btrfs subvolume create /mnt/@home
+umount /mnt
+mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@root /sda2 /mnt
+mkdir /mnt/{home}
+mount -o compress=zstd,noatimespace_cache=v2,subvol=@home /dev/sda2 /mnt/home
+mkdir /boot/efi
+mount /dev/sda1 /mnt/boot/efi
+#pacstrap /mnt base base-devel btrfs-progs efibootmgr intel-ucode iucode-tool nano linux-tkg-muqss-generic_v3 linux-tkg-muqss-generic_v3-headers
