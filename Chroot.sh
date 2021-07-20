1467803927aedf48c1908227ed93367171e06caf
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-21 00:32:55 +0300

    Update Chroot.sh

--- Chroot.sh
+++ Chroot.sh
@@ -12,5 +12,7 @@ passwd
 pacman -S grub efibootmgr
 useradd -m -g users -G wheel -s /bin/bash nikita
 passwd nikita
+mkdir /boot/efi 
+mount /dev/sda1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi --removable
 grub-mkconfig -o /boot/grub/grub.cfg
