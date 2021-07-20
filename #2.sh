893b33efd4e47a2235152b2a19af97a2a77ba402
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-21 00:44:59 +0300

    Update #2.sh

--- #2.sh
+++ #2.sh
@@ -1,6 +1,6 @@
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=ru_RU.UTF-8" > /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc --utc
 date
