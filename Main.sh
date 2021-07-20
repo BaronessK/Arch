71ac0addb6f3c8c90093468a4e45c5a9139ef00d
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-21 00:08:12 +0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -1,1 +1,1 @@
-pacstrap /mnt base base-devel btrfs-progs efibootmgr intel-ucode iucode-tool linux-tkg-bmqss-generic_v3 linux-tkg-bmqss-generic_v3-headers
+pacstrap /mnt base base-devel btrfs-progs efibootmgr intel-ucode iucode-tool linux-tkg-muqss-generic_v3 linux-tkg-muqss-generic_v3-headers
