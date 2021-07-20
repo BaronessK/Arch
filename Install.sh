979185f6180065c46259b6c67bf16dad8fef3d74
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-20 23:11:18 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,5 +1,4 @@
-#sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com & pacman-key --lsign-key 3056513887B78AEB
-#pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo umount /dev/sda2
 mkfs.fat -F32 /dev/sda1
 mkfs.btrfs -f /dev/sda2
 mount /dev/sda2 /mnt
@@ -11,4 +10,4 @@ mkdir /mnt/home
 mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@home /dev/sda2 /mnt/home
 mkdir /boot/efi
 mount /dev/sda1 /mnt/boot/efi
-#pacstrap /mnt base base-devel btrfs-progs efibootmgr intel-ucode iucode-tool nano linux-tkg-muqss-generic_v3 linux-tkg-muqss-generic_v3-headers
+
