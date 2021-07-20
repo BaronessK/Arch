acdf3a9cb34d94f04af189db1c594cb3fbf16ccd
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-20 23:04:17 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -7,7 +7,6 @@ btrfs subvolume create /mnt/@root
 btrfs subvolume create /mnt/@home
 umount /mnt
 mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@root /dev/sda2 /mnt
-mkdir /mnt/{home}
 mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@home /dev/sda2 /mnt/home
 mkdir /boot/efi
 mount /dev/sda1 /mnt/boot/efi
