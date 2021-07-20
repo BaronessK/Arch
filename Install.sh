5969fc16cd1e3d6a6fdbb6703b9969ccd58907f7
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-20 23:45:23 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,23 +1,10 @@
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com | 
-pacman-key --lsign-key 3056513887B78AEB | pacman-key --lsign-key 3056513887B78AEB
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com  
 pacman-key --lsign-key 3056513887B78AEB
 pacman-key --lsign-key 3056513887B78AEB
-pacman-key --lsign-key 3056513887B78AEB |
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+pacman-key --lsign-key 3056513887B78AEB
+pacman-key --lsign-key 3056513887B78AEB 
+pacman-key --lsign-key 3056513887B78AEB 
+pacman-key --lsign-key 3056513887B78AEB 
+pacman-key --lsign-key 3056513887B78AEB 
 clear
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
