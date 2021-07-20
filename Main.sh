4f0d4f066fc9197cab3f1da4c0f2e7492f5b3d2a
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-21 00:10:17 +0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -1,2 +1,3 @@
 pacstrap /mnt base base-devel btrfs-progs efibootmgr intel-ucode iucode-tool linux-tkg-muqss-generic_v3 linux-tkg-muqss-generic_v3-headers
 genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt /bin/bash
