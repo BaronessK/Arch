984a6262d5db82562a6f7a393f39b27ea279233a
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-21 00:32:38 +0300

    Update Prepare.sh

--- Prepare.sh
+++ Prepare.sh
@@ -6,8 +6,6 @@ umount /mnt
 mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@root /dev/sda2 /mnt 
 mkdir /mnt/home 
 mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@home /dev/sda2 /mnt/home
-mkdir /boot/efi 
-mount /dev/sda1 /boot/efi
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com  
 pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
