2bf662f5f6542bdf3a6789fa17f2a27383fcca40
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-21 00:34:09 +0300

    Delete Main.sh

--- Main.sh
+++
@@ -1,3 +0,0 @@
-pacstrap /mnt base base-devel intel-ucode iucode-tool linux-tkg-muqss-generic_v3 linux-tkg-muqss-generic_v3-headers git
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash
