08d837fd8168147b4b2e35c9a988182b5deff47e
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-21 01:16:25 +0300

    Update #2.sh

--- #2.sh
+++ #2.sh
@@ -21,6 +21,7 @@ mkdir /boot/efi
 mount /dev/sda1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi --removable
 grub-mkconfig -o /boot/grub/grub.cfg
+pacman -Suuyy
 exit
 umount -R /mnt
 
