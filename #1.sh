32a026c9c3dcceff6173817b3c18a60dbdff8ae4
Baronessk0-19 <87723745+Baronessk0-19@users.noreply.github.com>
2021-07-21 13:28:28 +0300

    Update #1.sh

--- #1.sh
+++ #1.sh
@@ -1,11 +1,11 @@
-mkfs.fat -F32 /dev/nvme0n1p5 & mkfs.btrfs -f /dev/nvme0n1p6
-mount /dev/nvme0n1p6 /mnt
+mkfs.fat -F32 /dev/sda1 & mkfs.btrfs -f /dev/sda2
+mount /dev/sda2 /mnt
 btrfs subvolume create /mnt/@root 
 btrfs subvolume create /mnt/@home 
 umount /mnt
-mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@root /dev/nvme0n1p6 /mnt 
+mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@root /dev/sda2 /mnt 
 mkdir /mnt/home 
-mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@home /dev/nvme0n1p6 /mnt/home
+mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@home /dev/sda2 /mnt/home
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com  
 pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
