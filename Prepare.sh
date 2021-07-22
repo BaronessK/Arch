dabad5328f6f594fe7a3ade233a8a1e2b8e7eaf3
BaronessK <87723745+BaronessK@users.noreply.github.com>
2021-07-22 09:09:55 +0300

    Update Prepare.sh

--- Prepare.sh
+++ Prepare.sh
@@ -2,10 +2,8 @@ mkfs.fat -F32 /dev/sda1
 mkfs.btrfs -f /dev/sda2 
 mkfs.btrfs -f /dev/sda3
 mount -o compress=zstd,noatime,space_cache=v2,ssd /dev/sda2 /mnt
-umount /mnt
 mkdir /mnt/home
 mount -o compress=zstd,noatime,space_cache=v2,ssd /dev/sda3 /mnt/home
-umount /mnt/home
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com  
