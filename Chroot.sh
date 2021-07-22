a586aeccbedbc202fbd1a78607c16ea2559d049a
BaronessK <87723745+BaronessK@users.noreply.github.com>
2021-07-22 07:02:06 +0300

    Update Chroot.sh

--- Chroot.sh
+++ Chroot.sh
@@ -6,7 +6,7 @@ passwd & useradd -m -g users -G wheel,video -s /bin/bash nikita & sh -c "sed -i 
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com  
 pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'& echo -e "[options] \n HoldPkg = pacman glibc \n Architecture = auto \n CheckSpace \n ParallelDownloads = 3 \n SigLevel = Required DatabaseOptional \n LocalFileSigLevel = Optional \n [core] \n Include = /etc/pacman.d/mirrorlist \n [extra] \n Include = /etc/pacman.d/mirrorlist \n [community] \n Include = /etc/pacman.d/mirrorlist \n [chaotic-aur] \n Include = /etc/pacman.d/chaotic-mirrorlist \n [multilib] \n Include = /etc/pacman.d/mirrorlist" > /etc/pacman.conf
-pacman -Suy wpa_supplicant dhclient iw pipewire nano git
+pacman -Suy wpa_supplicant dhclient iw pipewire nano git budgie-desktop lightdm
 systemctl enable dhcpcd.service
 bootctl install & echo "default Arch" > /boot/loader/loader.conf & echo -e "title BlackArch \n linux /vmlinuz-linux \n initrd /initramfs-linux.img \n options root=/dev/nvme0n1p6 rw
 exit
