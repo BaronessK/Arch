4d158a4c181f2af02c58c81c792864df118a5f81
BaronessK <87723745+BaronessK@users.noreply.github.com>
2021-07-22 06:55:50 +0300

    Update Chroot.sh

--- Chroot.sh
+++ Chroot.sh
@@ -8,7 +8,7 @@ pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'& echo -e "[options] \n HoldPkg = pacman glibc \n Architecture = auto \n CheckSpace \n ParallelDownloads = 3 \n SigLevel = Required DatabaseOptional \n LocalFileSigLevel = Optional \n [core] \n Include = /etc/pacman.d/mirrorlist \n [extra] \n Include = /etc/pacman.d/mirrorlist \n [community] \n Include = /etc/pacman.d/mirrorlist \n [chaotic-aur] \n Include = /etc/pacman.d/chaotic-mirrorlist \n [multilib] \n Include = /etc/pacman.d/mirrorlist" > /etc/pacman.conf
 pacman -Suy wpa_supplicant dhclient iw pipewire nano git
 systemctl enable dhcpcd.service
-bootctl install & echo "default Arch" > /boot/loader/loader.conf & echo -e "title BlackArch \n linux /vmlinuz-linux \n initrd /initramfs-linux.img \n options root=/dev/sda2 rw
+bootctl install & echo "default Arch" > /boot/loader/loader.conf & echo -e "title BlackArch \n linux /vmlinuz-linux \n initrd /initramfs-linux.img \n options root=/dev/nvme0n1p6 rw
 exit
 umount -R /mnt/boot & umount -R /mnt
 
