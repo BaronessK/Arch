ff0ce6024d64010bd533a2d4103f86336f79815a
BaronessK <87723745+BaronessK@users.noreply.github.com>
2021-07-22 13:36:32 +0300

    Update Prepare.sh

--- Prepare.sh
+++ Prepare.sh
@@ -1,12 +1,12 @@
-mkfs.fat -F32 /dev/sda1 
-mkfs.btrfs -f /dev/sda2 
-mount /dev/sda2 /mnt
+mkfs.fat -F32 /dev/nvme0n1p5 
+mkfs.btrfs -f /dev/nvme0n1p6 
+mount /dev/nvme0n1p6 /mnt
 btrfs subvolume create /mnt/@root
 btrfs subvolume create /mnt/@home
 umount /mnt
-mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@root /dev/sda2 /mnt
+mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@root /dev/nvme0n1p6 /mnt
 mkdir /mnt/{boot,home}
-mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@home /dev/sda2 /mnt/home
+mount -o compress=zstd,noatime,space_cache=v2,ssd,subvol=@home /dev/nvme0n1p6 /mnt/home
 mount /dev/sda1 /mnt/boot
 pacstrap /mnt base base-devel linux-firmware linux linux-headers git nano
 genfstab -U /mnt >> /mnt/etc/fstab
