c576f8904f0af53b37ca8b797a5916679fcd9a8e
BaronessK <87723745+BaronessK@users.noreply.github.com>
2021-07-22 06:39:41 +0300

    Delete Chaotic-AUR.sh

--- Chaotic-AUR.sh
+++
@@ -1,4 +0,0 @@
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com  
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo -e "[options] \n HoldPkg = pacman glibc \n Architecture = auto \n CheckSpace \n ParallelDownloads = 3 \n SigLevel = Required DatabaseOptional \n LocalFileSigLevel = Optional \n [core] \n Include = /etc/pacman.d/mirrorlist \n [extra] \n Include = /etc/pacman.d/mirrorlist \n [community] \n Include = /etc/pacman.d/mirrorlist \n [chaotic-aur] \n Include = /etc/pacman.d/chaotic-mirrorlist \n [multilib] \n Include = /etc/pacman.d/mirrorlist" > /etc/pacman.conf
